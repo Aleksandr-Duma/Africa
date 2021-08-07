d17ef731dd2232a4772f8390253e1f70e19e475f
Aleksandr_Duma <Dumaweb9@gmail.com>
2021-08-07 16:46:59 +0300

    add anchors on the buttons

--- docs/js/main.js
+++ docs/js/main.js
@@ -1,4 +1,15 @@
 $(function(){
-/*----------------------Animate.js--------------------------*/
+// ....................Animate.js......................
+
     new WOW().init();
+
+// ......................Scroll........................
+
+    $('a[href^="#"]').click(function(){
+        var target = $(this).attr('href');
+        $('html, body').animate({
+            scrollTop: $(target).offset().top
+        },800);
+    });
+    
 });
