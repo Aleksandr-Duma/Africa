9497b1fcdcbf81015c6a52dcd4eb2b5fa4dd8457
Aleksandr_Duma <Dumaweb9@gmail.com>
2021-08-10 14:29:06 +0300

    layout changes

--- docs/js/main.js
+++ docs/js/main.js
@@ -11,5 +11,30 @@ $(function(){
             scrollTop: $(target).offset().top
         },800);
     });
+
+// ...................For FAQ accordeon................
+
+    const faqTitle = document.querySelectorAll('.faq__item-title');
+
+    for(let i = 0; i < faqTitle.length; i++){
+        let faqContent = faqTitle[i].nextElementSibling;
+
+        faqTitle[i].addEventListener('click', function(){
+            faqTitle[i].classList.toggle('open');
+            faqContent.classList.toggle('active');
+        });
+    };
+
+// ...................For button top....................
+
+    const scrollBtn = document.querySelector('.button-up');
+
+    window.onscroll = function(){
+        if(window.scrollY > 600){
+            scrollBtn.classList.remove('hiden');
+        }else if(window.scrollY < 600){
+            scrollBtn.classList.add('hiden');
+        }
+    }
     
 });
